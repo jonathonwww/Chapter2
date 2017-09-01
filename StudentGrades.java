48951f26b63f95aeb8f33a5a6176b6ff3c8d4dbb
jonathonwww <30909315+jonathonwww@users.noreply.github.com>
2017-08-31 17:00:20 -0700

    Add files via upload

---
+++ StudentGrades.java
@@ -0,0 +1,21 @@
+/**
+ * This program is used to add up each student's lab and bonus points.
+ * @author Jonathon Webster
+ * @version August 31, 2017
+ */
+public class StudentGrades {
+
+		public static void main(String[] args) {
+
+			System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" );
+			System.out.println("==\t    Student Points\t    ==");
+			System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+			System.out.println("\nName\t\tLab\tBonus\tTotal");
+			System.out.println("----\t\t---\t-----\t-----");
+			System.out.println("Jonathon\t" + 45 + "\t" + 23 + "\t" + (45 + 23));
+			System.out.println("Sally\t\t" + 36 + "\t" + 17 + "\t" + (36 + 17));
+			System.out.println("Michael\t\t" + 46 + "\t" + 22 + "\t" + (46 + 22));
+			System.out.println("Jessica\t\t" + 56 + "\t" + 37 + "\t" + (56 + 37));
+			System.out.println("Steve\t\t" + 0 + "\t" + 2 + "\t" + (0 + 2));
+		}
+}		
