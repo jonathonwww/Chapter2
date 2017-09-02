400e6a7de9dd0fae36c63e4455fd7ea176b13115
jonathonwww <30909315+jonathonwww@users.noreply.github.com>
2017-09-02 10:56:46 -0700

    Create Paint.java

---
+++ Paint.java
@@ -0,0 +1,52 @@
+//***************************************************************
+//File: Paint.java
+//
+//Purpose: Determine how much paint is needed to paint the walls
+//of a room given its length, width, and height
+//***************************************************************
+
+import java.util.Scanner;
+public class Paint
+{
+	public static void main(String[] args)
+	{
+		final int COVERAGE = 350; //paint covers 350 sq ft/gal
+		
+		int length, width, height;
+		//declare integers length, width, and height;
+		
+		double TotalSqFt;
+		//declare double totalSqFt;
+		
+		double paintNeeded;
+		//declare double paintNeeded;
+		
+		Scanner scan = new Scanner(System.in);
+		//declare and initialize Scanner object
+		
+		System.out.print("Length: ");
+		length = scan.nextInt();
+		//Prompt for and read in the length of the room
+		
+		System.out.print("\nWidth: ");
+		width = scan.nextInt();
+		//Prompt for and read in the width of the room
+		
+		System.out.print("\nHeight: ");
+		height = scan.nextInt();
+		//Prompt for and read in the height of the room
+		
+		TotalSqFt =  (width * height * 2) + (length * height * 2);
+		
+		System.out.println("\nYour total area is  " + TotalSqFt + " feet squared.");
+		/*Compute the total square feet to be painted--think
+		about the dimensions of each wall*/
+		
+		paintNeeded = Math.round((TotalSqFt/350) * 100.0 ) / 100.0;
+		//Compute the amount of paint needed
+		
+		System.out.println("You need " + paintNeeded + " gallons of paint.");
+		//Print the length, width, and height of the room and the
+		//number of gallons of paint needed.
+	}
+}	
